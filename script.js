cd26f8c693fc7c5bcb55ae4ca4f948449b81a69c
shreyash honwadajkar <shreyashhonwadajkar11@gmail.com>
2025-11-27 02:57:39 +0530

    Update Firebase SDK and configuration details
    Updated Firebase SDK version and configuration.

--- script.js
+++ script.js
@@ -3,16 +3,29 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// --- PASTE YOUR FIREBASE CONFIG BELOW ---
-// Replace the lines inside the brackets with YOUR keys from the Firebase Console
-const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAUm-RHq4naN9P_5muKOcxZbeNnZZYE3uo",
+    authDomain: "skyshout-edf4d.firebaseapp.com",
+    projectId: "skyshout-edf4d",
+    storageBucket: "skyshout-edf4d.firebasestorage.app",
+    messagingSenderId: "273663944072",
+    appId: "1:273663944072:web:957fe61a4a7d302195c63d",
+    measurementId: "G-1TSCXJWWDP"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
