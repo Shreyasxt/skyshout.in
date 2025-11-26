f134ac6625edf8d373a14f7401e46cfac5762812
shreyash honwadajkar <shreyashhonwadajkar11@gmail.com>
2025-11-27 02:27:47 +0530

    Add Google login and payment redirection functions

---
+++ script.js
@@ -0,0 +1,8 @@
+function googleLogin() {
+  document.getElementById('google-login-overlay').style.display = 'none';
+}
+
+function redirectPayment() {
+  alert("Redirecting to Payment Gateway...");
+  // You can add Razorpay or Stripe integration code here later
+}
